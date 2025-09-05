7138e10663484371b64c248b46e10f7ee84736fd
Bowie <marketing@letscode.com.br>
2025-09-04 23:21:20 -0300

    subindo novos testes do desafio 2

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,8 +1,16 @@
-const { somarDoisNumeros } = require('../src/calculadora');
+// O "expect" é usado para verificar se o resultado obtido é igual ao esperado
+
+// Importamos as funções da calculadora que vamos testar
+// Essas funções foram definidas em outro arquivo (../src/calculadora.js)
+const {somarDoisNumeros} = require('../src/calculadora');
+
+// Importamos a função "expect" do framework Chai
 const { expect } = require('chai');
 
-
+// O "describe" serve para agrupar testes relacionados
 describe('Testes da Função de Soma', function () {
+
+
     it('A função deve ser capaz de somar dois números positivos', function () {
         // Coleta o resultado da função
         const resultadoDaSoma = somarDoisNumeros(5, 3);
@@ -18,7 +26,27 @@ describe('Testes da Função de Soma', function () {
         // Compara o resultado com o valor que você espera
         expect(resultadoDaSoma).to.equal(35);
     });
+
+    it('A função deve ser capaz de somar dois numeros zeros', function () {
+        // Coleta o resultado da função
+        const resultadoDaSoma = somarDoisNumeros(0,0);
+
+        // Compara o resultado com o valor que você espera
+        expect(resultadoDaSoma).to.equal(0);
+    });
+
+    it('A função deve ser capaz de somar dois números negativos', function () {
+        // Coleta o resultado da função
+        const resultadoDaSoma = somarDoisNumeros(-20, -15);
+
+        // Compara o resultado com o valor que você espera
+        expect(resultadoDaSoma).to.equal(-35);
+
+    });
+
 });
+
+
 
 
 
